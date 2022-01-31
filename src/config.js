dec560f64d741240457051c1fae98d8f1de36740
falklinus <33982429+falklinus@users.noreply.github.com>
2022-01-31 13:37:11 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
   authentication: {
     jwtSecret:
       process.env.JWT_SECRET ||
-      '44870eccc64b6ab1437f12724dfc17479f0dd7d892a817d1acd8673c83802103fb1365e6b782da9cd12e746c32a9e45584e8531c8250489621a5215e0bac5429',
+      'jwtSecret',
   },
-  fixerKey: process.env.FIXER_KEY || '45b434d233095b21a3291fee9c1727a0',
+  fixerKey: process.env.FIXER_KEY || 'fixerKey',
 }
